20b1575ab4ebd5a4a83af2dfe29ddb9bba928019
Sergey Royz <zjor.se@gmail.com>
2022-02-18 18:00:43 +0100

    auth0 module: wip

--- cli/auth.py
+++ cli/auth.py
@@ -3,8 +3,13 @@ import time
 import dotenv
 import requests
 import logging
+import colorama
+from termcolor import colored
 from http.client import HTTPConnection
+from cli.auth0_client import Auth0Client
 from cli.auth0_protocol import GetDeviceCodeResponse, GetAccessTokenResponse, GetUserInfoResponse
+
+colorama.init()
 
 dotenv.load_dotenv()
 
@@ -26,48 +31,10 @@ def enable_debug_logging():
     HTTPConnection.debuglevel = 1
 
 
-def get_device_code():
-    scopes = ['name', 'email', 'profile', 'openid', 'offline_access']
-    url = f"{AUTH0_BASE_URL}/oauth/device/code"
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    data = {
-        'client_id': AUTH0_CLIENT_ID,
-        'scope': ' '.join(scopes),
-        'audience': AUTH0_AUDIENCE
-    }
-
-    response = requests.post(url, data=data, headers=headers)
-    return GetDeviceCodeResponse.from_json(response.json())
-
-
-def get_access_token(device_code):
-    url = f"{AUTH0_BASE_URL}/oauth/token"
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    data = {
-        'client_id': AUTH0_CLIENT_ID,
-        'grant_type': 'urn:ietf:params:oauth:grant-type:device_code',
-        'device_code': device_code
-    }
-    response = requests.post(url, data=data, headers=headers)
-    return response.status_code, GetAccessTokenResponse.from_json(response.json())
-
-
-def get_user_info(access_token):
-    url = f"{AUTH0_BASE_URL}/userinfo"
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-        'Content-Type': 'application/json'
-    }
-    response = requests.get(url, headers=headers)
-    return response.status_code, GetUserInfoResponse.from_json(response.json())
-
-
 if __name__ == "__main__":
-    device_code_response = get_device_code()
+    auth0_client = Auth0Client.get_instance()
+
+    device_code_response = auth0_client.get_device_code()
     print(f"Device code: {device_code_response.device_code}")
     print(f"Please visit URL: {device_code_response.verification_uri_complete}")
 
@@ -76,13 +43,13 @@ if __name__ == "__main__":
         attempts += 1
         time.sleep(device_code_response.interval)
         print(f"Waiting for the device approval. Attempt: {attempts}")
-        code, access_token_response = get_access_token(device_code_response.device_code)
+        code, access_token_response = auth0_client.get_access_token(device_code_response.device_code)
         if attempts >= 15 or code == 200:
             break
 
     if code == 200:
         print(f"Access token: {access_token_response.access_token}")
-        code, user_info = get_user_info(access_token_response.access_token)
+        code, user_info = auth0_client.get_user_info(access_token_response.access_token)
         if user_info.error:
             print(f"Failed to get user info: {user_info.error} - {user_info.error_description}")
         else:
---
+++ cli/auth0_client.py
@@ -0,0 +1,62 @@
+import os
+import dotenv
+import requests
+from cli.auth0_protocol import GetDeviceCodeResponse, GetAccessTokenResponse, GetUserInfoResponse
+
+dotenv.load_dotenv()
+
+AUTH0_AUDIENCE = os.getenv('AUTH0_AUDIENCE')
+AUTH0_CLIENT_ID = os.getenv('AUTH0_CLIENT_ID')
+AUTH0_BASE_URL = os.getenv('AUTH0_BASE_URL')
+
+
+class Auth0Client:
+    __instance = None
+
+    @staticmethod
+    def get_instance():
+        if not Auth0Client.__instance:
+            Auth0Client.__instance = Auth0Client(AUTH0_BASE_URL, AUTH0_CLIENT_ID, AUTH0_AUDIENCE)
+        return Auth0Client.__instance
+
+    def __init__(self, base_url, client_id, audience):
+        self.base_url = base_url
+        self.client_id = client_id
+        self.audience = audience
+
+    def get_device_code(self):
+        scopes = ['name', 'email', 'profile', 'openid', 'offline_access']
+        url = f"{self.base_url}/oauth/device/code"
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        data = {
+            'client_id': self.client_id,
+            'scope': ' '.join(scopes),
+            'audience': self.audience
+        }
+
+        response = requests.post(url, data=data, headers=headers)
+        return GetDeviceCodeResponse.from_json(response.json())
+
+    def get_access_token(self, device_code):
+        url = f"{self.base_url}/oauth/token"
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        data = {
+            'client_id': self.client_id,
+            'grant_type': 'urn:ietf:params:oauth:grant-type:device_code',
+            'device_code': device_code
+        }
+        response = requests.post(url, data=data, headers=headers)
+        return response.status_code, GetAccessTokenResponse.from_json(response.json())
+
+    def get_user_info(self, access_token):
+        url = f"{self.base_url}/userinfo"
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Content-Type': 'application/json'
+        }
+        response = requests.get(url, headers=headers)
+        return response.status_code, GetUserInfoResponse.from_json(response.json())
